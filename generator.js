8c467dcd2540aa996344be69a9b1e6b290f7a3c6
hxl <2713>
2021-08-02 16:33:49 +0800

    .

--- generator.js
+++ generator.js
@@ -2,8 +2,8 @@ module.exports = api => {
 	api.extendPackage({
 		scripts: {
 			'mpm-create': 'vue-cli-service mpm-create',
-			'mpm-delete': 'vue-cli-service mpm-delete',
-			'mpm-move': 'vue-cli-service mpm-move',
+			// 'mpm-delete': 'vue-cli-service mpm-delete',
+			// 'mpm-move': 'vue-cli-service mpm-move',
 		}
 	})
 }
